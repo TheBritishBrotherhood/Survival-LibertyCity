71792193287f8e486406ba38d524e39f8fe61a46
Doofy Gilmore <curtislee1994@live.co.uk>
2021-03-07 08:48:48 -0800

    map 4

---
+++ server-data/resources/[map]/[LC_V101_FM]/[NJ]/jersey_metadata/__resource.lua
@@ -0,0 +1,57 @@
+this_is_a_map 'yes'
+file 'gtxd.meta'
+data_file 'GTXD_PARENTING_DATA' 'gtxd.meta'
+
+file 'nj_01.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'nj_01.ytyp'
+
+file 'nj_01_lod.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'nj_01_lod.ytyp'
+
+file 'nj_02.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'nj_02.ytyp'
+
+file 'nj_02_lod.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'nj_02_lod.ytyp'
+
+file 'nj_03.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'nj_03.ytyp'
+
+file 'nj_03_extra.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'nj_03_extra.ytyp'
+
+file 'nj_03_extra_lod.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'nj_03_extra_lod.ytyp'
+
+file 'nj_03_lod.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'nj_03_lod.ytyp'
+
+file 'nj_04e.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'nj_04e.ytyp'
+
+file 'nj_04e_lod.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'nj_04e_lod.ytyp'
+
+file 'nj_04w.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'nj_04w.ytyp'
+
+file 'nj_04w_lod.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'nj_04w_lod.ytyp'
+
+file 'nj_05.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'nj_05.ytyp'
+
+file 'nj_05_lod.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'nj_05_lod.ytyp'
+
+file 'nj_docks.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'nj_docks.ytyp'
+
+file 'nj_docks_lod.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'nj_docks_lod.ytyp'
+
+file 'nj_liberty.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'nj_liberty.ytyp'
+
+file 'nj_liberty_lod.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'nj_liberty_lod.ytyp'
